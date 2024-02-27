2378651990144e0084d8fa41780d90b1c7eebedf
JuneMutten <junemutten@gmail.com>
2024-02-27 03:38:07 -0800

    Betty and compiling for the first 11 tasks

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,29 +4,28 @@
  * binary_tree_insert_left - Inserts a node as the left-child of another node
  * @parent: Poiter to the node to insert left-child in
  * @value: Value to store in the new node
- * @Return: Pointer to the created node, otherwise NULL.
-            If parent already has a left-child, the new node must take its place, and the old left-child must be set as the left-child of the new node.
+ * Return: Pointer to the created node, otherwise NULL.
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    if (parent == NULL)
-        return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-    new_node = malloc(sizeof(binary_tree_t));
+	new_node = binary_tree_node(parent, value);
 
-    if (new_node == NULL)
-        return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-    if (parent->left != NULL)
-    {
-        new_node->left = parent->left;
-        parent->left->parent = new_node;
-    }
+	if (parent->left != NULL)
+	{
+		new_node->left = parent->left;
+		parent->left->parent = new_node;
+	}
 
-    parent->left = new_node;
+	parent->left = new_node;
 
-    return (new_node);
+	return (new_node);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,21 +7,23 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        return 0;
-    }
-    else
-    {
-        size_t left_height = binary_tree_height(tree->left);
-        size_t right_height = binary_tree_height(tree->right);
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		size_t left_height = binary_tree_height(tree->left);
+		size_t right_height = binary_tree_height(tree->right);
 
-        if (left_height > right_height)
-        {
-            return (left_height + 1);
-        }
-        else
-        {
-            return (right_height + 1);
-        }
-    }
+		if (left_height > right_height)
+		{
+			return (left_height + 1);
+		}
+
+		else
+		{
+			return (right_height + 1);
+		}
+	}
+}
