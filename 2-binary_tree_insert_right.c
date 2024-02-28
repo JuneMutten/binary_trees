1e7218c69d893a8f76bfdb5d555b6fb2bd0aca9a
JuneMutten <junemutten@gmail.com>
2024-02-27 23:51:33 -0800

    Betty style task 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -4,29 +4,28 @@
  * binary_tree_insert_right - Inserts a node as the right-child of another node
  * @parent: Poiter to the node to insert right-child in
  * @value: Value to store in the new node
- * @Return: Pointer to the created node, otherwise NULL.
-            If parent already has a right-child, the new node must take its place, and the old right-child must be set as the right-child of the new node.
+ * Return: Pointer to the created node, otherwise NULL
  */
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
+	binary_tree_t *new_node;
 
-    if (parent == NULL)
-        return (NULL);
+	if (parent == NULL)
+		return (NULL);
 
-    new_node = binary_tree_node(parent, value);
+	new_node = binary_tree_node(parent, value);
 
-    if (new_node == NULL)
-        return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-    if (parent->right != NULL)
-    {
-        new_node->right = parent->right;
-        parent->right->parent = new_node;
-    }
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
 
-    parent->right = new_node;
+	parent->right = new_node;
 
-    return (new_node);
+	return (new_node);
 }
