b69e95dea094a6632247623b0a67f9a6e94a4e75
JuneMutten <junemutten@gmail.com>
2024-02-29 01:31:14 -0800

    Batty and compilation for the last of the mandatory tasks

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,17 +7,17 @@
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-    size_t nodes = 0;
+	size_t nodes = 0;
 
-    if (!tree)
-        return (0);
+	if (!tree)
+		return (0);
 
-    else
-    {
-        nodes += (tree->left || tree->right) ? 1 : 0;
-        nodes += binary_tree_nodes(tree->left);
-        nodes += binary_tree_nodes(tree->right);
-    }
+	else
+	{
+		nodes += (tree->left || tree->right) ? 1 : 0;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+	}
 
-    return (nodes);
+	return (nodes);
 }
