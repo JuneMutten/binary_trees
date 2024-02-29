8c72ae22bbb6db80f8a3b3cdff39707dba380a29
JuneMutten <junemutten@gmail.com>
2024-02-29 01:57:11 -0800

    Correcting task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -7,7 +7,7 @@
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	if (!tree)
+	if (tree)
 		return (0);
 
 	int left_height = binary_tree_height(tree->left);
@@ -35,3 +35,4 @@ size_t binary_tree_height(const binary_tree_t *tree)
         }
         return (0);
 }
+~
